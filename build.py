dc9ccb0827b1cb846e04ab7c2598671d486be15d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-11-17 16:49:25 -0300

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
 from conan.packager import ConanMultiPackager
 
-
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="testing")
+    builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
     builder.add_common_builds(shared_option_name="libzmq:shared")
+
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
+                and not options["libzmq:shared"]:
+
+            filtered_builds.append([settings, options, env_vars, build_requires])
+
+    builder.builds = filtered_builds
     builder.run()
