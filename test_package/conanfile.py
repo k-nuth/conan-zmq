76ed3dc6f78885ea07fc314c879a62298b2c7e7b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-11-21 10:49:43 -0300

    fix

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,7 +10,8 @@ class ZMQTestConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
+        # cmake = CMake(self.settings)
+        cmake = CMake(self)
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
